516839e6e079585bc69bf3c48c8cce84e366c3fd
Jesus Calderin <jesuscalderin91@gmail.com>
2022-03-01 16:28:39 -0400

    fix: dotenv only on dev

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const http = require("http");
 const httpProxy = require("http-proxy");
-require('dotenv').config();
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const PORT = process.env.PORT || 8080;
-const target = process.env.TARGET_URL
-console.log({target});
+const target = process.env.TARGET_URL;
+console.log({ target });
 httpProxy.createProxyServer({ target }).listen(PORT);
-console.log('Running')
+console.log("Running");
